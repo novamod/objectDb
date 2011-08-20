d4bfc154c2d7743346b150fbbeda0c83c1de78e1
dornova <dor.nova@gmail.com>
2011-08-20 12:24:38 -0400

    form saturn

--- src/tutorial/Hello.java
+++ src/tutorial/Hello.java
@@ -8,6 +8,8 @@ public class Hello {
 		System.out.println("Hello World!");
 		
 		System.out.println("this is a new message from tom walker");
+		
+		System.out.println("hello world from saturn from tom walker");
 	}
 
 }
